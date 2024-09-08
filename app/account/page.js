dce93e4cab1ed68cfe5f73e46e3aaa70277ec07c
Pourushg210 <pourushgupta@gmail.com>
2024-09-09 01:26:50 +0530

    Added nested routing for account and reservation pages and desgned them

--- app/account/page.js
+++ app/account/page.js
@@ -1,13 +1,11 @@
-import Navigation from "../_components/navigation";
-
 export const metadata = {
   title: "Guest Area",
 };
 
 const Page = () => {
   return (
-    <div>
-      <h1>Creating new Account here</h1>
+    <div className="font-semibold text-2xl text-accent-400 mb-7">
+      <h1>Welcome, Pourush</h1>
     </div>
   );
 };
