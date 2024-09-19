97a7d4c1abc946fc721797988ead9ca411d9c455
Pourushg210 <pourushgupta@gmail.com>
2024-09-20 01:13:39 +0530

    added notFound page and other things

--- app/about/page.js
+++ app/about/page.js
@@ -16,22 +16,22 @@ export default function Page() {
 
         <div className="space-y-8">
           <p>
-            Where nature&apos;s beauty and comfortable living blend seamlessly.
-            Hidden away in the heart of the Italian Dolomites, this is your
-            paradise away from home. But it&apos;s not just about the luxury
-            cabins. It&apos;s about the experience of reconnecting with nature
-            and enjoying simple pleasures with family.
+            Where nature beauty and comfortable living blend seamlessly. Hidden
+            away in the heart of the Italian Dolomites, this is your paradise
+            away from home. But it is not just about the luxury cabins. It is
+            about the experience of reconnecting with nature and enjoying simple
+            pleasures with family.
           </p>
           <p>
             Our 8 luxury cabins provide a cozy base, but the real freedom and
-            peace you&apos;ll find in the surrounding mountains. Wander through
+            peace you will find in the surrounding mountains. Wander through
             lush forests, breathe in the fresh air, and watch the stars twinkle
             above from the warmth of a campfire or your hot tub.
           </p>
           <p>
-            This is where memorable moments are made, surrounded by
-            nature&apos;s splendor. It&apos;s a place to slow down, relax, and
-            feel the joy of being together in a beautiful setting.
+            This is where memorable moments are made, surrounded by nature
+            splendor. It is a place to slow down, relax, and feel the joy of
+            being together in a beautiful setting.
           </p>
         </div>
       </div>
@@ -67,12 +67,12 @@ export default function Page() {
             dedication to creating a warm, welcoming environment.
           </p>
           <p>
-            Over the years, we&apos;ve maintained the essence of The Wild Oasis,
+            Over the years, we have maintained the essence of The Wild Oasis,
             blending the timeless beauty of the mountains with the personal
-            touch only a family business can offer. Here, you&apos;re not just a
-            guest; you&apos;re part of our extended family. So join us at The
-            Wild Oasis soon, where tradition meets tranquility, and every visit
-            is like coming home.
+            touch only a family business can offer. Here, you are not just a
+            guest; you are part of our extended family. So join us at The Wild
+            Oasis soon, where tradition meets tranquility, and every visit is
+            like coming home.
           </p>
 
           <div>
